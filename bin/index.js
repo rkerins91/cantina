65d33f6de3b93af140e96c126580291b3a963d3b
Ryan Kerins <ryanakerins@gmail.com>
2019-11-04 19:08:39 -0500

    completed attempt at multi selector solution

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 const select = require('./select')
+const multiSelect = require('./multiSelect')
 const data = require('./data')
+
 
 // call select function with argument and assign it to variable selected
 const selected = select(process.argv[2], data)
 
-
+const multi = multiSelect(selected, process.argv.slice(3))
 // log values of returned array
-selected.forEach((ele, idx) => {
+multi.forEach((ele, idx) => {
   console.log(`${idx + 1}. ${JSON.stringify(ele, null, 2)}`)
   return
 })
@@ -16,5 +18,5 @@ selected.forEach((ele, idx) => {
 
 // log length of value returned by calling select function with arg
 console.log(
-  `There are ${selected.length} occcurances that apply to your query`
+  `There are ${multi.length} occcurances that apply to your query`
 )
---
+++ bin/multiSelect.js
@@ -0,0 +1,44 @@
+
+const multiSelect = (possibleResults, selectors) => {
+  // Base case will be that there are no more selectors
+  let matchedResults = []
+  const selector = selectors[0]
+  if (selectors.length === 0) {
+
+    return possibleResults
+  }
+
+  // Otherwise we have some new selector to look for a match in our possibleResults array i.e
+  // looping through possibleResults and checking appropriate properties for results
+
+  // if classNames has match 
+  else if (selector[0] === '.') {
+    matchedResults = possibleResults.filter((ele, idx) => {
+      return ele.classNames.includes(selector.slice(1))
+    })
+  }
+
+  // if identifier has match 
+  else if (selector[0] === '/') {
+    matchedResults = possibleResults.filter((ele, idx) => {
+      return ele.identifier === selector.slice(1)
+    })
+  }
+
+  // else we have a class selector
+
+  else {
+    matchedResults = possibleResults.filter((ele, idx) => {
+      return ele.identifier === selector
+    })
+  }
+
+  // Now that matchedResults has been built up from our selector and possibleResults, we can
+  // recursively call multiSelect to check the remaining selectors with the new possibleResults
+  
+
+  return multiSelect(matchedResults, selectors.slice(1))
+  
+}
+
+module.exports = multiSelect
