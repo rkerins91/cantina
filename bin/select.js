45e326767067f68fd574faf8314f544cc2012540
Ryan Kerins <ryanakerins@gmail.com>
2019-11-06 12:24:32 -0500

    changed "control" control to only push control property

--- bin/select.js
+++ bin/select.js
@@ -16,32 +16,32 @@ const select = (selector, data) => {
 const parse = (data, selectorName, selectorType) => {
 
   let result = []
-
+  // check if match is found, first checking if prop exists, then if it's className
   const matchFound = data[selectorType] &&
     selectorType === 'classNames' ?
     data[selectorType].includes(selectorName) :
     data[selectorType] === selectorName
-    
+  // push data to result
   if (matchFound) {
     result.push(data)
   }
-
+  // if data has control and control object has selectorType prop equal to name
   if (data.control && data.control[selectorType] === selectorName) {
-    result.push(data)
+    result.push(data.control)
   }
-
+  // if data is an array, loop through array recursively calling parse on each element
   if (Array.isArray(data)) {
     for (let i = 0; i < data.length; i++) {
       result = result.concat(parse(data[i], selectorName, selectorType))
     }
   }
-
+  // if data has subviews, loop through subviews array recursively calling parse on each element
   if (data.subviews) {
     for (let i = 0; i < data.subviews.length; i++) {
       result = result.concat(parse(data.subviews[i], selectorName, selectorType))
     }
   }
-
+  // if data has contentView, loop through all properties recursively calling parse on each key
   if (data.contentView) {
     for (let key in data.contentView) {
       result = result.concat(parse(data.contentView[key], selectorName, selectorType))
